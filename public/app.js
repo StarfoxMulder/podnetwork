b12921acec9a02ad8ae25572cdfa432d0432b665
cprestia1 <cprestia1@lenovo.com>
2017-10-18 19:36:38 -0400

    pushing -- better color scheme and dropdown menu works on hover

--- public/app.js
+++ public/app.js
@@ -1,5 +1,23 @@
-$( document ).ready(function() {
-  $(".dropdown-button").dropdown(
-    { hover: true }
-  );
+
+$(document).ready(function(){
+   $(".dropdown-button").dropdown({
+        hover: true
+    });
 });
+
+
+/*
+
+<ul class="nav navbar-nav">
+  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown1" href="/members">Members<i class="material-icons right">arrow_drop_down</i></a>
+    <ul id="dropdown1" class="dropdown-content">
+      <li><a href="/unbelievable">Unbelievable Podcast</a></li>
+      <li><a href="/zengthis">Zeng This!</a></li>
+      <li><a href="/stp">Secret Transmission Podcast</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Page 2</a></li>
+  <li><a href="#">Page 3</a></li>
+</ul>
+
+*/
